70d9b5c4c12d886c23711f639f15efa12046c7e9
M. Ridvan Korurer <m.ridvankorurer@gmail.com>
2026-09-17 00:08:58 +0300

    api v5

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // 1. Axios Instance Oluşturma
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://elisi-fxey.onrender.com',
   withCredentials: true, // HttpOnly Cookie'lerin (JWT) otomatik gönderilmesini sağlar
   headers: {
     'Content-Type': 'application/json'
