7f8e1191e765608d85ef31064e61ca96591398c6
M. Ridvan Korurer <m.ridvankorurer@gmail.com>
2026-09-17 00:20:27 +0300

    api v7

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
-app.options('*', cors());
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
